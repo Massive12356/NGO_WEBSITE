f73a624bc3db3c6a412c27034e1abc5fd06641f3
Massive12356 <edwardmintah17@gmail.com>
2025-10-20 11:00:07 +0000

    chnages made

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "", // empty string ensures correct relative paths
   build: {
     outDir: "dist",
   },
